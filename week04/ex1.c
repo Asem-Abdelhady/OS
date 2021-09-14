8788822fd7aa68affd7943e10d4dc9bd73e998f9
Asem-Abdelhady <asemshawkey@gmail.com>
2021-09-14 23:27:51 +0300

    Add: exercise 1 explanation as a comment

--- week04/ex1.c
+++ week04/ex1.c
@@ -4,7 +4,7 @@
 int main() {
 
     int n = fork();
-
+    // when forked the child process get 0 indicator while the parent process still the same
     if(n == 0){
         printf("Hello from child [%d, %d]\n", getpid(), n);
     }
